34b3765e061ed4fa08bc3ec137dd85dc20fcfba9
ilyass <79806692+illyasshaddoud@users.noreply.github.com>
2024-02-06 17:38:04 +0100

    fixing jwt secret

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest-starter'),
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,7 +9,7 @@ import { JwtModule } from '@nestjs/jwt';
     UsersModule,
     JwtModule.register({
       global: true,
-      secret: 'moknadya',
+      secret: '',
     }),
   ],
   controllers: [AuthController],
